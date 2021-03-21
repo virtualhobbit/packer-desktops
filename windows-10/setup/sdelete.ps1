df60f171acb1fcebf493dd4ff38abd70661cfaec
Mark Brookfield <mark@mdb-lab.com>
2021-03-21 23:14:17 +0100

    Revised

--- windows-10/setup/sdelete.ps1
+++ windows-10/setup/sdelete.ps1
@@ -5,35 +5,31 @@ $zip = "SDelete.zip"
 $exe = "sdelete64.exe"
 $arg = "-z c: /accepteula"
 
-# Verify connectivity
-$connTestResult = Test-NetConnection -Computername $webserver -Port 80
-if ($connTestResult.TcpTestSucceeded){
-  # Get SDelete
-  Invoke-WebRequest -Uri ($url + "/" + $zip) -OutFile C:\$zip
+# Get SDelete
+Invoke-WebRequest -Uri ($url + "/" + $zip) -OutFile C:\$zip
 
-  # Unzip it
-  Expand-Archive -LiteralPath "C:\$zip" -DestinationPath C:\ -Confirm:$false
+# Unzip it
+Expand-Archive -LiteralPath "C:\$zip" -DestinationPath C:\ -Confirm:$false
 
-  # Run SDelete
-  Try
-  {
-    Start-Process C:\$exe -ArgumentList $arg -PassThru -Wait -ErrorAction Stop
-  }
-  Catch
-  {
-    Write-Error "Failed to run SDelete"
-    Write-Error $_.Exception
-    Exit -1
-  }
-
-  # Delete files
-  [Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
-  $source = [IO.Compression.ZipFile]::OpenRead("C:\$zip")
-  $entries = $source.Entries
-  ForEach ($file in $entries)
-  {
-    Remove-Item -Path C:\$file -Confirm:$false
-  }
-  $source.Dispose()
-  Remove-Item C:\$zip -Confirm:$false
+# Run SDelete
+Try 
+{
+   Start-Process C:\$exe -ArgumentList $arg -PassThru -Wait -ErrorAction Stop
 }
+Catch
+{
+   Write-Error "Failed to run SDelete"
+   Write-Error $_.Exception
+   Exit -1 
+}
+
+# Delete files
+[Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
+$source = [IO.Compression.ZipFile]::OpenRead("C:\$zip")
+$entries = $source.Entries
+ForEach ($file in $entries)
+{
+    Remove-Item -Path C:\$file -Confirm:$false
+}
+$source.Dispose()
+Remove-Item C:\$zip -Confirm:$false
