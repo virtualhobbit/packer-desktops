62e9bbdc20466cd043c025a284e86f4519738043
Mark Brookfield <mark@mdb-lab.com>
2021-03-21 23:14:52 +0100

    zzz

--- windows-10/setup/sdelete.ps1
+++ windows-10/setup/sdelete.ps1
@@ -12,24 +12,21 @@ Invoke-WebRequest -Uri ($url + "/" + $zip) -OutFile C:\$zip
 Expand-Archive -LiteralPath "C:\$zip" -DestinationPath C:\ -Confirm:$false
 
 # Run SDelete
-Try 
-{
-   Start-Process C:\$exe -ArgumentList $arg -PassThru -Wait -ErrorAction Stop
+Try {
+  Start-Process C:\$exe -ArgumentList $arg -PassThru -Wait -ErrorAction Stop
 }
-Catch
-{
-   Write-Error "Failed to run SDelete"
-   Write-Error $_.Exception
-   Exit -1 
+Catch {
+  Write-Error "Failed to run SDelete"
+  Write-Error $_.Exception
+  Exit -1 
 }
 
 # Delete files
 [Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
 $source = [IO.Compression.ZipFile]::OpenRead("C:\$zip")
 $entries = $source.Entries
-ForEach ($file in $entries)
-{
-    Remove-Item -Path C:\$file -Confirm:$false
+ForEach ($file in $entries){
+  Remove-Item -Path C:\$file -Confirm:$false
 }
 $source.Dispose()
 Remove-Item C:\$zip -Confirm:$false
