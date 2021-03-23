23ffbb133e83e1698957a7a1635f5c577892baba
Mark Brookfield <mark@mdb-lab.com>
2021-03-23 20:45:03 +0100

    Changed Agent version to 21.03 (8.2)

--- windows-10/setup/agent.ps1
+++ windows-10/setup/agent.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop"
 
 $webserver = "intranet.mdb-lab.com"
 $url = "http://" + $webserver
-$installer = "VMware-Horizon-Agent-x86_64-2012-8.1.0-17352461.exe"
+$installer = "VMware-Horizon-Agent-x86_64-2103-8.2.0-17771933.exe"
 $listConfig = "/s /v ""/qn REBOOT=ReallySuppress ADDLOCAL=Core,NGVC,RTAV,ClientDriveRedirection,V4V,VmwVaudio,PerfTracker"""
 
 # Verify connectivity
