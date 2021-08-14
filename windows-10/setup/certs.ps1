a2fb7f36267f148bacc8952a947f298a1ce20e9d
Mark Brookfield <mark@mdb-lab.com>
2021-08-14 15:50:55 +0200

    Changed certs script

--- windows-10/setup/certs.ps1
+++ windows-10/setup/certs.ps1
@@ -2,25 +2,24 @@ $ErrorActionPreference = "Stop"
 
 $webserver = "intranet.mdb-lab.com"
 $url = "http://" + $webserver
-$certRoot = "root64.crt"
-$certIssuing = "issuing.crt"
+$certRoot = "rootCA.crt"
+$certIssuing = "issuingCA.crt"
 
 # Verify connectivity
-$connTestResult = Test-NetConnection -Computername $webserver -Port 80
-if ($connTestResult.TcpTestSucceeded){
-  # Get certificates
-  ForEach ($cert in $certRoot,$certIssuing) {
-    Invoke-WebRequest -Uri ($url + "/" + $cert) -OutFile C:\$cert
-  }
+Test-Connection $webserver -Count 1
 
-  # Import Root CA certificate
-  Import-Certificate -FilePath C:\$certRoot -CertStoreLocation 'Cert:\LocalMachine\Root'
+# Get certificates
+ForEach ($cert in $certRoot,$certIssuing) {
+  Invoke-WebRequest -Uri ($url + "/" + $cert) -OutFile C:\$cert
+}
 
-  # Import Issuing CA certificate
-  Import-Certificate -FilePath C:\$certIssuing -CertStoreLocation 'Cert:\LocalMachine\CA'
+# Import Root CA certificate
+Import-Certificate -FilePath C:\$certRoot -CertStoreLocation 'Cert:\LocalMachine\Root'
 
-  # Delete certificates
-  ForEach ($cert in $certRoot,$certIssuing) {
-    Remove-Item C:\$cert -Confirm:$false
-  }
+# Import Issuing CA certificate
+Import-Certificate -FilePath C:\$certIssuing -CertStoreLocation 'Cert:\LocalMachine\CA'
+
+# Delete certificates
+ForEach ($cert in $certRoot,$certIssuing) {
+  Remove-Item C:\$cert -Confirm:$false
 }
