3c8c0721305bc38f64b4f0442b9f7bea49f68172
Mark Brookfield <mark@mdb-lab.com>
2021-09-01 19:02:09 +0200

    Added Windows 11

---
+++ windows-11/setup/agent.ps1
@@ -0,0 +1,28 @@
+$ErrorActionPreference = "Stop"
+
+$webserver = "intranet.mdb-lab.com"
+$url = "http://" + $webserver
+$installer = "VMware-Horizon-Agent-x86_64-2106-8.3.0-18287218.exe"
+$listConfig = "/s /v ""/qn REBOOT=ReallySuppress ADDLOCAL=Core,NGVC,RTAV,ClientDriveRedirection,V4V,VmwVaudio,PerfTracker"""
+
+# Verify connectivity
+$connTestResult = Test-NetConnection -Computername $webserver -Port 80
+if ($connTestResult.TcpTestSucceeded){
+  # Get Horizon Agent
+  Invoke-WebRequest -Uri ($url + "/" + $installer) -OutFile C:\$installer
+
+  # Unblock installer
+  Unblock-File C:\$installer -Confirm:$false -ErrorAction Stop
+
+  # Install Horizon Agent
+  Try {
+    Start-Process C:\$installer -ArgumentList $listConfig -PassThru -Wait -ErrorAction Stop
+  } Catch {
+    Write-Error "Failed to install the Horizon Agent"
+    Write-Error $_.Exception
+    Exit -1
+  }
+
+  # Cleanup on aisle 4...
+  Remove-Item C:\$installer -Confirm:$false
+}
